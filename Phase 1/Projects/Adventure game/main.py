66125d69be037437f0655032fb79cd46befe97c9
Dawood Afzal <dawoodafzal.62138@gmail.com>
2026-06-12 11:27:51 +0500

    Basic GUI made  , Gemini connection needed

--- Phase 1/Projects/Adventure game/main.py
+++ Phase 1/Projects/Adventure game/main.py
@@ -1,4 +1,14 @@
-import json 
+import json
+import pathlib
+
+
+
+SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
+FILE_PATH = SCRIPT_DIR / "sample.json"
+
+
+
+
 
 
 class player_state:
@@ -7,5 +17,29 @@ class player_state:
 
 
 
-class 
+class game_state:
+    pass
+
+
+
+
+
+
+
+with FILE_PATH.open("r") as f:
+    data =json.load(f)
+
+
+# print(json.dumps(data , indent= 4 ))
+
+
+for key ,  vlaue in data.items():
+    print(key , vlaue)
+
+player= player_state()
+
+
+
+
+setattr(player ,)
 
