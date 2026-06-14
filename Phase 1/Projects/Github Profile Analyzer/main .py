8ac9a072bbb20941a0795a1248616d81647cc70c
Dawood Afzal <dawoodafzal.62138@gmail.com>
2026-06-14 19:52:41 +0500

    GitHub profile analysis Started !

---
+++ Phase 1/Projects/Github Profile Analyzer/main .py
@@ -0,0 +1,35 @@
+
+import pathlib
+import json
+class profile_analyzer:
+    def __init__(self, username , Path , Url ):
+        self.username=  username
+        self.path = Path
+        self.url=Url
+
+
+
+
+    def getrepos(self):
+        pass
+
+    def calculate_stars(self):
+        pass
+
+
+
+
+
+def get_username():
+    pass
+
+
+
+
+dir = pathlib.Path(__file__).parent.resolve() / "reports.json"
+url=""
+
+
+
+if __name__ == "__main__":
+    pass
