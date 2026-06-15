0ca8755e0a1208c4040f00d212227e7b9358bd3a
Dawood Afzal <dawoodafzal.62138@gmail.com>
2026-06-15 19:53:08 +0500

    get_user_profile function done

--- Phase 1/Projects/Github Profile Analyzer/main .py
+++ Phase 1/Projects/Github Profile Analyzer/main .py
@@ -18,15 +18,44 @@ PATH = pathlib.Path(__file__).parent.resolve() / "reports.json"
 
 
 class profile_analyzer:
-    def __init__(self, username , Path , Url ):
+    def __init__(self, username , Path , Url  , HEADERS):
         self.path = Path
         self.url=Url
         self.username= username
+        self.HEADERS=HEADERS
 
+        
 
+    
+    def get_response(self , url ):
+        response = requests.get(url ,headers=self.HEADERS).json()
+        return response
 
     def get_user_profile(self):
-        pass
+        profile_url = self.url + f"users/{self.username}"
+        response = self.get_response(profile_url)
+        print("\n" + "=" * 50)
+        print("GITHUB USER PROFILE")
+        print("=" * 50)
+
+        print(f"Name                : {response['name']}")
+        print(f"User ID             : {response['id']}")
+        print(f"Location            : {response['location']}")
+        print(f"Company             : {response['company']}")
+        print(f"Email               : {response['email']}")
+        print(f"Twitter Username    : {response['twitter_username']}")
+
+        print("\n--- Account Information ---")
+        print(f"Profile URL         : {response['html_url']}")
+        print(f"Followers           : {response['followers']}")
+        print(f"Public Repositories : {response['public_repos']}")
+
+        print("\n--- Activity Information ---")
+        print(f"Account Created On  : {response['created_at'].split('T')[0]}")
+        print(f"Last Updated On     : {response['updated_at'].split('T')[0]}")
+
+        print("=" * 50) 
+
 
 
     def calculate_repository_stats(self):
@@ -47,7 +76,7 @@ def get_username():
     pass
 
 
-url="https://api.github.com/users/dawoodafzal62138/repos"
+# url="https://api.github.com/users/dawoodafzal62138/repos"
 url="https://api.github.com/"
 
 
@@ -55,11 +84,13 @@ url="https://api.github.com/"
 r= requests.get(url,headers=HEADERS).json()
 
 
-with PATH.open("w") as f:
-    json.dump(r , f, indent=4 )
+# with PATH.open("w") as f:
+#     json.dump(r , f, indent=4 )
 
 
 
 
-# if __name__ == "__main__":
-#     pass
+if __name__ == "__main__":
+    pa= profile_analyzer("dawoodafzal62138", PATH , url , HEADERS)
+    pa.get_user_profile()
+
