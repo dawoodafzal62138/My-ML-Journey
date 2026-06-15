a9b189623d3fc47890e1b60bd531fbba9d504235
Dawood Afzal <dawoodafzal.62138@gmail.com>
2026-06-15 13:12:09 +0500

    Add Base functions to make

--- Phase 1/Projects/Github Profile Analyzer/main .py
+++ Phase 1/Projects/Github Profile Analyzer/main .py
@@ -1,40 +1,65 @@
-
+import os
 import pathlib
 import json
 import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+HEADERS = { 
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"Bearer {GITHUB_TOKEN}",
+    "X-GitHub-Api-Version": "2026-03-10"
+   }
+PATH = pathlib.Path(__file__).parent.resolve() / "reports.json"
+
+
+
+
 class profile_analyzer:
     def __init__(self, username , Path , Url ):
-        self.username=  username
         self.path = Path
         self.url=Url
+        self.username= username
 
 
 
-
-    def getrepos(self):
-        pass
-
-    def calculate_stars(self):
+    def get_user_profile(self):
         pass
 
 
+    def calculate_repository_stats(self):
+        pass
 
+
+    def get_repository_languages(self, repo_name):
+        pass
+
+    def language_stats(self):
+        pass
+
+    def analyze_commits(self):
+        pass
 
 
 def get_username():
     pass
 
 
-
-
-dir = pathlib.Path(__file__).parent.resolve() / "reports.json"
-url="https://api.github.com/repos/dawoodafzal62138/issues"
-
+url="https://api.github.com/users/dawoodafzal62138/repos"
+url="https://api.github.com/"
 
 
 
-r= requests.get(url)
-print(r)
+r= requests.get(url,headers=HEADERS).json()
+
+
+with PATH.open("w") as f:
+    json.dump(r , f, indent=4 )
+
+
+
 
 # if __name__ == "__main__":
 #     pass
