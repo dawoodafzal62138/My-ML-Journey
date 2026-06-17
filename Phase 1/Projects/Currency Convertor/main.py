3d0f12e85d7235aed90fcae439660b7fefbdc49d
Dawood Afzal <dawoodafzal.62138@gmail.com>
2026-06-17 19:17:31 +0500

    [Currency Convertor] Currency Convertor Project Started !

---
+++ Phase 1/Projects/Currency Convertor/main.py
@@ -0,0 +1,33 @@
+import requests 
+import json
+from halo import Halo
+import sys
+import time
+
+class currecny_convertor:
+    def __init__(self , API_key , base_url):
+        pass
+
+
+    def view_supported_currencies():
+        pass
+
+    def Convert_Currency():
+        pass
+
+
+
+
+
+
+
+
+
+Base_url = ""
+API_KEY="4191ef346b1638d65d318a2c"
+
+
+
+
+if __name__ == "__main__":
+    pass
