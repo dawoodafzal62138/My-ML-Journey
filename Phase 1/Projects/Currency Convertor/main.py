b059e8f8ab1fd3424506af2264072a37ac873619
Dawood Afzal <dawoodafzal.62138@gmail.com>
2026-06-18 22:10:20 +0500

    [ Currency Convertor] Readme Added Project Completed  : )

--- Phase 1/Projects/Currency Convertor/main.py
+++ Phase 1/Projects/Currency Convertor/main.py
@@ -1,38 +1,50 @@
 import requests 
-import json
 from halo import Halo
-import sys
-import time
 import os
-
-def typewritter(text , delay = 0.01):
-    for char in text:
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        time.sleep(delay)
+import sys
 
 
 
-class currecny_convertor:
+
+
+class currency_convertor:
     def __init__(self , base_url):
         self.url = base_url
 
 
     def get_response(self , url):
+        spinner = Halo("Fetching Codes ", "cyan", "blue" ,spinner="dots3")
+        spinner.start()
         try:
             response = requests.get(url).json()
+            
+            if "error-type" in response.keys():
+                return f"\tError :\033[31m  {response['error-type']}\n" 
             return response
-        except Exception as e:
-            return {}
+
+        except requests.exceptions.MissingSchema:
+            return "Error: Invalid URL (missing http/https)"
+
+        except requests.exceptions.ConnectionError:
+            return "Error: Cannot connect to server"
+
+        except requests.exceptions.Timeout:
+            return "Error: Request timed out"
+
+        except requests.exceptions.HTTPError as e:
+            return f"HTTP Error: {e}"
+
+        except ValueError:
+            return "Error: Response is not valid JSON"
+        finally:
+            spinner.stop()
+
 
 
     def view_supported_currencies(self):
         supported_cuurency_url = self.url + "/codes"
-        spinner = Halo("Fetching Codes ", "cyan", "blue" ,spinner="dots3")
-        spinner.start()
         response = self.get_response(supported_cuurency_url)
-        spinner.stop()
-        if response:
+        if isinstance(response , dict):
             col = 3
             for index , codes in enumerate(response["supported_codes"] ,start=1):
                 print(f"\033[32m [{index:>3}]  \033[1;34m{codes[0]} \033[0m: \033[36m{codes[1]:<30}" , end="")
@@ -41,34 +53,39 @@ class currecny_convertor:
                     print()
             print()
         else:
-            typewritter("Something Wrong Happened :(")
+            print(response)
 
 
 
 
-    def Convert_Currency(self, from_, to  , amount):
-        conversion_ur = self.url +f"pair/{from_.upper().strip()}/{to.upper().strip()}"
-        response  = self.get_response(conversion_ur)
-        rate = response.get("conversion_rate")
-        total = amount * rate
+    def convert_currency(self):
+        from_= input("  \033[97m[1]\033[0m  \033[97m Enter the source Currency Code  : \033[0m").upper().strip()
+        to = input("  \033[97m[2]\033[0m  \033[97m Enter the target Currency Code  : \033[0m").upper().strip()
+        while True:
+            try:
+                amount = float(input("  \033[97m[3]\033[0m  \033[97m Enter the Amount                : \033[0m"))
+                if amount > 0:
+                    conversion_url = self.url +f"/pair/{from_.upper().strip()}/{to.upper().strip()}"
+                    response  = self.get_response(conversion_url)
+                    if isinstance(response , dict):
+                            rate = response.get("conversion_rate")
+                            total = amount * rate
 
-        print("\n\033[34m" + "-" * 45 + "\033[0m")
-        
-        print(f"  \033[90mExchange Rate :\033[0m 1 \033[97m{from_}\033[0m = \033[96m{round(rate, 4)} {to}\033[0m")
-        
-        print(f"  \033[90mConversion    :\033[0m \033[97m{amount} {from_}\033[0m = \033[32m{round(total, 2)} {to}\033[0m")
-        
-        print("\033[34m" + "-" * 45 + "\033[0m\n")
+                            print("\n\033[34m" + "-" * 45 + "\033[0m")
+                            
+                            print(f"  \033[90mExchange Rate :\033[0m 1 \033[97m{from_}\033[0m = \033[96m{round(rate, 4)} {to}\033[0m")
+                            
+                            print(f"  \033[90mConversion    :\033[0m \033[97m{amount} {from_}\033[0m = \033[32m{round(total, 2)} {to}\033[0m")
+                            
+                            print("\033[34m" + "-" * 45 + "\033[0m\n")
+                    else:
+                        print(response)
+                    break
+                else:
+                    print("\n\033[31m \tAmount Can't be Negative\n")
+            except ValueError:
+                print("\t\033[31m Invalid Input !")
 
-
-
-
-
-
-
-
-API_KEY="4191ef346b1638d65d318a2c"
-Base_url = f"https://v6.exchangerate-api.com/v6/{API_KEY}"
 
 
 def menu():
@@ -104,22 +121,26 @@ def menu():
 
 
 
-
-   
-
-
 if __name__ == "__main__":
-    c_obj = currecny_convertor(Base_url)
-    # # c_obj.view_supported_currencies()
-    # # print("\033[32m index. \033[1;34m code \033[0m: \033[36m currency ")
-    # c_obj.Convert_Currency("usd", "pkr" , 3.10)
+    # The API key should be enclosed in String
+    API_KEY="Your-API-Key"
+
+    Base_url = f"https://v6.exchangerate-api.com/v6/{API_KEY}"
+    
+    c_obj = currency_convertor(Base_url)
     action = { 
         "1": c_obj.view_supported_currencies,
-        "2": c_obj.Convert_Currency,
-        "3": exit
+        "2": c_obj.convert_currency,
+        "3": sys.exit
     }
-                    
+    while True:
+        choice = menu()
+        if choice in action.keys():
+            action[choice]()
+            input("\n  \033[34m Press Enter to return to menu...")
+        else: 
+            print("\033[31m     Invalid Choice !")     
 
 
 
-    menu()
+   
