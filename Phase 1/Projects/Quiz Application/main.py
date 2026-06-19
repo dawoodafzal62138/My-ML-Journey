07770b740f026afe814d9b8ede5ea03c4b008134
Dawood Afzal <dawoodafzal.62138@gmail.com>
2026-06-19 22:45:10 +0500

    [Quiz APP] menu function added

--- Phase 1/Projects/Quiz Application/main.py
+++ Phase 1/Projects/Quiz Application/main.py
@@ -1,9 +1,9 @@
 import json
-import requests
 from halo import Halo
 import os
 from google import genai
 from dotenv import load_dotenv
+import sys
 
 
 # Load the api key from gemini
@@ -72,7 +72,7 @@ class quiz_app:
 
 
 
-def get_difficulty():
+def get_difficulty_subject():
     subjects = [
     "General Knowledge",
     "Science",
@@ -90,11 +90,75 @@ def get_difficulty():
     "Art & Culture",
     "Mixed Quiz"
 ]
+    
     difficulty = ["hard" , "medium" ,"easy"]
+    while True:
+        try:
+            os.system("cls" if os.name == "nt" else "clear")
+            print("\033[34;1mSelect Subject > \n")
+            for index , item in enumerate(subjects, start=1):
+                print(f"\033[32m [{index}]  \033[37m{item}")
+            choice= int(input("\t\n\033[32mEnter your Choice > "))
+            if choice > 0 and choice <= len(subjects):
+                selected_subject = subjects[choice-1]
+                break
+            else:
+                print("\n\033[31m Invalid Choice ")
+        except:
+            print("\n \033[31 Invalid Choice")
+    while True:
+        try:
+            os.system("cls" if os.name == "nt" else "clear")
+            print("\033[34;1mSelect Difficulty > \n")
+            for index , item in enumerate(difficulty, start=1):
+                print(f"\033[32m [{index}]  \033[37m{item}")
+            choice= int(input("\t\n\033[32mEnter your Choice > "))
+            if choice > 0 and choice <= len(subjects):
+                selected_difficulty = difficulty[choice-1]
+                break
+            else:
+                print("\n\033[31m Invalid Choice ")
+        except:
+            print("\n\033[31 Invalid Choice")
+
+    return selected_difficulty ,selected_subject
+
+
+
+def menu():
+    os.system("cls" if os.name == "nt" else "clear")
+    print(r"""
+ ██████╗ ██╗   ██╗██╗███████╗     █████╗ ██████╗ ██████╗ 
+██╔═══██╗██║   ██║██║╚══███╔╝    ██╔══██╗██╔══██╗██╔══██╗
+██║   ██║██║   ██║██║  ███╔╝     ███████║██████╔╝██████╔╝
+██║▄▄ ██║██║   ██║██║ ███╔╝      ██╔══██║██╔═══╝ ██╔═══╝ 
+╚██████╔╝╚██████╔╝██║███████╗    ██║  ██║██║     ██║     
+ ╚══▀▀═╝  ╚═════╝ ╚═╝╚══════╝    ╚═╝  ╚═╝╚═╝     ╚═╝     
+""")
+    print("- "*30)
+    print("\033[36m\t\tLearn -- Play -- Improve")
+    print("- "*30)
+    print("\033[34m [1] \033[90mStart Quiz")
+    print("\033[34m [2] \033[90mSee History")
+    print("\033[34m [3] \033[90mExit")
+    choice= input("\t\n\033[32mEnter your Choice > ")
+    return choice
+
+
+
+
+
+
 
     
 if __name__ == "__main__":
 
-
-
+    # difficulty , subject = get_difficulty_subject()
+    # print(difficulty, subject)
+    # menu()
     quiz = quiz_app()
+    actions={
+        "1" : quiz.start_quiz,
+        "2" : quiz.see_history,
+        "3" : sys.exit
+    }
