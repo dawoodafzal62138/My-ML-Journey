26083b1429f8623dce494e41e45dee8346db7081
Dawood Afzal <dawoodafzal.62138@gmail.com>
2026-06-24 18:59:12 +0500

    [To Do CLI App] basic functionality done : )

--- Phase 1/Projects/To do CLI App/main.py
+++ Phase 1/Projects/To do CLI App/main.py
@@ -12,12 +12,21 @@ class Task:
         self.status = status
         self.created_at = created_at
 
+    def to_dict(self):
+        data = {
+            "id" : self.id,
+            "title" : self.title,
+            "status" : self.status,
+            "created_at" : self.created_at
+        }
+        return data
 
 
 class TaskManager:
     def __init__(self , path : str):
         self.path = path
 
+    
     def load_task(self):
         try:
 
@@ -34,15 +43,15 @@ class TaskManager:
             return 1
         return max([item["id"] for item in data]) + 1
 
+    
+    
     def add_task(self  , title : str, status : str , created_at ):
-
         id = self.get_next_id()
         t= Task( id , title , status, created_at )
-        self.data= vars(t)
+        self.data= t.to_dict()
         self.save_task()
 
-    
-    
+
     def save_task(self):
         data = self.load_task()
         data.append(self.data)
@@ -50,15 +59,35 @@ class TaskManager:
             json.dump(data, f ,indent=4)
 
 
-    def delete_task(self):
-        pass
-    def compelete_task(self):
-        pass
+    def delete_task(self  ,id : int):
+        data = self.load_task()
+        for item in data:
+            if item["id"] == id:
+                data.remove(item)
+        with self.path.open("w" ) as f:
+            json.dump(data, f ,indent=4)
+        
+    
 
+    def compelete_task(self , id ):
+        data = self.load_task()
+        for item in data:
+            if item["id"] == id:
+                item["status"] = "completed"
+        with self.path.open("w" ) as f:
+            json.dump(data, f ,indent=4)
 
-
+    
+    
     def list_tasks(self):
-        pass
+        data = self.load_task()
+        for item in data:
+            print("="*60)
+            for key , value in item.items():
+                print(f"{key } : {value}")
+            print("="*60)
+
+
 
 
 
@@ -66,4 +95,7 @@ PATH  = pathlib.Path(__file__).parent.resolve() / "data.json"
 
 if __name__ == "__main__":
     tm = TaskManager(PATH)
-    tm.add_task( "buy groceries"  , "pending" , str(datetime.datetime.now()))
+    # tm.add_task( "buy groceries"  , "pending" , str(datetime.datetime.now()))
+    # tm.delete_task(1)
+    # tm.compelete_task(2)
+    tm.list_tasks()
