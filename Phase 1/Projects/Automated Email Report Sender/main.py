8915dda0f8d7b0669b780d42edae9bdce1300b1b
Dawood Afzal <dawoodafzal.62138@gmail.com>
2026-06-26 20:30:52 +0500

    [Automated Email Report Sender] organize function completed

--- Phase 1/Projects/Automated Email Report Sender/main.py
+++ Phase 1/Projects/Automated Email Report Sender/main.py
@@ -9,11 +9,32 @@ class Data:
 
 
     def read_csv(self):
-        categories = []
+        filtered_data = []
         with self.path.open("r") as f:
             data  = csv.DictReader(f)
+            list_ = ["Product Category" ,"Units Sold" ,"Unit Price" ,"Total Revenue" ,"Region"]
             for line in list(data)[:32]:
+                category = {}
                 for key , value in line.items():
+                    if key in list_:
+                        category[key] = value
+                    
+                filtered_data.append(category)
+        return filtered_data 
+    
+    
+    def organize_data(self):
+        data = self.read_csv()
+
+        sorted_data = sorted(
+            data,
+            key=lambda item: (
+                item["Product Category"],
+                item["Region"]
+            )
+        )
+        return sorted_data
+    
 
 
     def response_ai(self):
@@ -30,5 +51,5 @@ class Data:
 if __name__ == "__main__":
     path = pathlib.Path(__file__).parent.resolve() / "Online Sales Data kaggle.csv"
     d = Data(path)
-    d.read_csv()
+    d.organize_data()
 
