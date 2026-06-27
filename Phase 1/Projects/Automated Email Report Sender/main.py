fddce2fc67d7c8d3e8876a87b0c0d6320fae13f2
Dawood Afzal <dawoodafzal.62138@gmail.com>
2026-06-27 18:49:35 +0500

    [Automated email report sender] all functions done ,  readme added

--- Phase 1/Projects/Automated Email Report Sender/main.py
+++ Phase 1/Projects/Automated Email Report Sender/main.py
@@ -1,11 +1,22 @@
 import csv
 import pathlib
-import json
+from google import genai
+from google.genai import errors
+from dotenv import load_dotenv
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 
 class Data:
-    def __init__(self , path ):
-        self.path = path 
+    def __init__(self , path , api , system_prompt):
+        self.path = path    
+        self.GEMINI_API = api
+        self.system_path = system_prompt
+        
+
 
 
     def read_csv(self):
@@ -38,10 +49,72 @@ class Data:
 
 
     def response_ai(self):
-        pass
+        data = self.organize_data()
+        if os.path.exists(self.system_path):
+            with self.system_path.open("r") as f:
+                prompt = f.read()
+        else:
+            prompt = ""
+
+
+        try:
+            client = genai.Client(api_key=self.GEMINI_API)
+            session = client.chats.create(model ="gemini-3.1-flash-lite" , config={"system_instruction": prompt})
+            response = session.send_message(str(data))
+    
+            return response.text
+        except errors.ClientError as e:
+            return f"Error: {e}"
 
 
 
+
+
+def send_email(APP_PASSWORD , response):
+    # 1. Email Configuration
+    smtp_server = "smtp.gmail.com"  # Example using Gmail
+    smtp_port = 587                 # Standard port for TLS
+    
+    # senders gmail address
+    sender_email = "example@gmail.com"
+    
+    sender_password = APP_PASSWORD # Use an App Password, not your real password!
+    
+    
+    
+    # recievers gmail address
+    receiver_email = "example@gmail.com"
+
+    # 2. Construct the Email Message
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    # Subject
+    message["Subject"] = "Daily Sales Report"
+
+    # Create the body of the email
+
+    body = response
+    message.attach(MIMEText(body, "plain"))
+
+    # 3. Connect to the Server and Send
+    try:
+        # Establish a secure session with the server
+        print("Connecting to server...")
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls() # Secure the connection
+            
+            # Login to your email account
+            server.login(sender_email, sender_password)
+            print("Login successful!")
+            
+            # Send the email
+            # We convert the MIME message object to a string before sending
+            server.sendmail(sender_email, receiver_email, message.as_string())
+            print("Email sent successfully!")
+            
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 
@@ -49,7 +122,17 @@ class Data:
 
 
 if __name__ == "__main__":
-    path = pathlib.Path(__file__).parent.resolve() / "Online Sales Data kaggle.csv"
-    d = Data(path)
-    d.organize_data()
+
+    load_dotenv()
+    GEMINI_API = os.getenv("GEMINI_API")
+    APP_PASSWORD = os.getenv("APP_PASSWORD")
+    
+    DATA_PATH = pathlib.Path(__file__).parent.resolve() / "Online Sales Data kaggle.csv"
+    PROMPT_PATH = pathlib.Path(__file__).parent.resolve() / "prompt.txt"
+
+    d = Data(DATA_PATH , GEMINI_API ,PROMPT_PATH)
+    response = d.response_ai()
+    send_email( APP_PASSWORD, response)
+
+
 
