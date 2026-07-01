30a5beec23d783e0a2331e21069c8ce21ed71155
Dawood Afzal <dawoodafzal.62138@gmail.com>
2026-07-01 18:23:10 +0500

    [Python Custom Testing Framework] add regex assert functions

--- Phase 1/Projects/Python_Custom_Testing_Framework/framework/__init__.py
+++ Phase 1/Projects/Python_Custom_Testing_Framework/framework/__init__.py
@@ -1,6 +1,6 @@
 
 from .decorators import test, skip, expect_failure
-from .assertions import assert_equal, assert_not_equal, assert_true, assert_false, assert_raises ,assert_is , assert_is_not , assert_is_none ,assert_is_not_none , assert_is_instance , assert_is_not_instance , assert_not_in , assert_in ,assert_almost_equal ,assert_not_almost_equal , assert_greater ,assert_greater_equal ,assert_less ,assert_less_equal
+from .assertions import assert_equal, assert_not_equal, assert_true, assert_false, assert_raises ,assert_is , assert_is_not , assert_is_none ,assert_is_not_none , assert_is_instance , assert_is_not_instance , assert_not_in , assert_in ,assert_almost_equal ,assert_not_almost_equal , assert_greater ,assert_greater_equal ,assert_less ,assert_less_equal ,assert_is_valid_regex , assert_not_regex , assert_regex 
 from .runner import TestRunner
 
 _runner = TestRunner()
