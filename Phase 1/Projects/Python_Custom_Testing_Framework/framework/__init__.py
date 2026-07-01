d8ecb1f082d5c47d0dd6f36361587ea55a74d5ac
Dawood Afzal <dawoodafzal.62138@gmail.com>
2026-07-01 16:48:48 +0500

    [Python Custom Testing Framework] include new assertion functions

--- Phase 1/Projects/Python_Custom_Testing_Framework/framework/__init__.py
+++ Phase 1/Projects/Python_Custom_Testing_Framework/framework/__init__.py
@@ -1,6 +1,6 @@
 
 from .decorators import test, skip, expect_failure
-from .assertions import assert_equal, assert_not_equal, assert_true, assert_false, assert_raises
+from .assertions import assert_equal, assert_not_equal, assert_true, assert_false, assert_raises ,assert_is , assert_is_not , assert_is_none ,assert_is_not_none
 from .runner import TestRunner
 
 _runner = TestRunner()
--- Phase 1/Projects/Python_Custom_Testing_Framework/framework/assertions.py
+++ Phase 1/Projects/Python_Custom_Testing_Framework/framework/assertions.py
@@ -1,27 +1,27 @@
 from .exceptions import TestFailed
 
 
-def assert_equal(actual , expected):
+def assert_equal(actual : object , expected : object) -> any:
     if actual != expected:
         raise TestFailed(f"Expected : {repr(expected)} , but Got : {repr(actual)}")
     
 
-def assert_not_equal(actual , expected):
+def assert_not_equal(actual : object , expected : object) -> any:
     if actual == expected:
         raise TestFailed(f"Expected : {repr(expected)} , but Got : {repr(actual)}")
 
 
-def assert_true(value : bool):
+def assert_true(value : bool) -> any:
     if not value :
         raise TestFailed(f"Expected truthy value , but Got : {repr(value)}")
 
 
-def assert_false(value : bool):
+def assert_false(value : bool)-> any:
     if value :
         raise TestFailed(f"Expected False value , but Got : {repr(value)}")
     
 
-def assert_raises(exception_type, func , *args , **kwargs):
+def assert_raises(exception_type  : Exception, func : function , *args , **kwargs) -> any:
     try:
         func(*args , **kwargs)
     except exception_type:
@@ -31,4 +31,37 @@ def assert_raises(exception_type, func , *args , **kwargs):
     
     else:
         raise TestFailed(f"Expected : {exception_type.__name__} to be raised , but Nothing raised")
-    
+
+
+
+
+
+def assert_is(expr1 : object , expr2 : object ) -> None:
+    if expr1 is not expr2 :
+        raise TestFailed("Expected two same objects , But Got different. ")
+
+
+
+
+
+def assert_is_not(expr1 : object , expr2 : object ) -> None:
+    if expr1 is expr2:
+        raise TestFailed("Expected two different objects , But Got Same. ")
+
+
+
+
+def assert_is_none(expr1 : object  ) -> None:
+    if expr1 is not None:
+        raise TestFailed(f"Expected None , But Got {expr1}. ")
+
+
+
+
+
+def assert_is_not_none(expr1 : object  ) -> None:
+    if expr1 is None:
+        raise TestFailed("Expected a value that is not None, but got None.")
+
+
+
