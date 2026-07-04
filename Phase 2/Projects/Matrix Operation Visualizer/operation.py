e02db1fcdb7e778fd9204b3e9e974221f0c550b8
Dawood Afzal <dawoodafzal.62138@gmail.com>
2026-07-04 20:40:27 +0500

    [Matrix Operation Visualizer] Add initial implementation

---
+++ Phase 2/Projects/Matrix Operation Visualizer/operation.py
@@ -0,0 +1,16 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+def multiply_step_by_step(matrix1 : NDArray , matrix2):
+    pass
+
+
+def inverse_step_by_step(matrix1 : NDArray , matrix2):
+    pass
+
+def transpose(matrix1 : NDArray , matrix2):
+    pass
+
+def get_determinent(matrix1 : NDArray , matrix2):
+    pass
---
+++ Phase 2/Projects/Matrix Operation Visualizer/visualizer.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+
+def draw_matrix():
+    pass
+
+def animate_multiplication():
+    pass
+def animate_transpose():
+    pass
+def animate_inverse():
+    pass
+
