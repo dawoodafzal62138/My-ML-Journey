e1f003a2609347b23af08290324138912fa1e1f8
Dawood Afzal <dawoodafzal.62138@gmail.com>
2026-07-09 16:57:00 +0500

    [Core Vectorized Operations] Activation function completed !

--- Phase 2/Projects/Core Vectorized Operations/Activation_functions.py
+++ Phase 2/Projects/Core Vectorized Operations/Activation_functions.py
@@ -3,17 +3,23 @@ import numpy as np
 
 
 
-def sigmoid():
-    pass
+def sigmoid(x : NDArray) -> NDArray:
+
+    return 1/(1+np.exp(-x))
+
 
 
 
 
 # Rectified linear unit
-def RLU():
-    pass
+def relu(x : NDArray) -> NDArray:
+    return np.maximum(0 , x)
 
 
-def softmax():
-    pass
+
+def softmax(x : NDArray) -> NDArray:
+    shifted_x = x - np.max(x, axis=-1, keepdims=True)
+    
+    exps = np.exp(shifted_x)
+    return exps / np.sum(exps, axis=-1, keepdims=True)
 
